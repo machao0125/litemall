e046e142b8e1edfa4e7da4a1439d9bc38d3bc4f4
machao <1761798982@qq.com>
2018-08-09 16:01:51 +0800

    修改接口默认参数

--- litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/wxController/MallContrloller.java
+++ litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/wxController/MallContrloller.java
@@ -1,10 +1,13 @@
 package org.linlinjava.litemall.wx.wxController;
 
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.linlinjava.litemall.core.util.ResponseUtil;
 import org.linlinjava.litemall.db.chao.domain.Mall;
 import org.linlinjava.litemall.db.chao.service.IMallService;
 import org.linlinjava.litemall.wx.annotation.LoginUser;
+import org.linlinjava.litemall.wx.web.WxAddressController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +22,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/wx/malls")
 public class MallContrloller {
+    private final Log logger = LogFactory.getLog(MallContrloller.class);
 
     @Autowired
     private IMallService mallService;
@@ -36,11 +40,13 @@ public class MallContrloller {
     }
 
     @PostMapping("mallListByUserId")
-    public Object mallListByUserId(@LoginUser Integer userId) {
+    public Object mallListByUserId( Integer userId) {
         //1、判断入参不为空
         if (userId == null) {
             return ResponseUtil.badArgument();
         }
+        logger.info("userId: " + userId);
+
         //2、调用service方法获取店铺列表
         Mall mall = mallService.mallListByUserId(userId);
 
